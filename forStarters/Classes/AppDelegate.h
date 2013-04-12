6af4356ffd01a10714a2ecd8de563b5b68710f49
Назар <nivanenko@gmail.com>
2013-04-12 12:20:01 +0300

    Add Push Notifications plugin

--- forStarters/Classes/AppDelegate.h
+++ forStarters/Classes/AppDelegate.h
@@ -29,6 +29,8 @@
 
 #import <Cordova/CDVViewController.h>
 
+#import "PushNotification.h" // <--- Added for push notifications
+
 @interface AppDelegate : NSObject <UIApplicationDelegate>{}
 
 // invoke string is passed to your app on launch, this is only valid if you
